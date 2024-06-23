723cf58a5ee5e9683cac781c4e71b2dd57412da0
Wazarr <jeremy.fraoua@gmail.com>
2024-06-23 15:29:08 +0200

    clean up

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 import numpy as np
 from haxballgym import make
@@ -16,39 +17,42 @@ from stable_baselines3.common.vec_env import VecCheckNan, VecMonitor, VecNormali
 from ursinaxball import Game
 from ursinaxball import common_values as cv
 
-if __name__ == "__main__":
-    frame_skip = 10
-    half_life_seconds = 5
+FRAME_SKIP = 10
+HALF_LIFE_SECONDS = 5
+NB_GAMES = 8
 
-    def create_gym_env(render: bool):
-        game = Game(
-            stadium_file=cv.BaseMap.CLASSIC,
-            logging_level=logging.DEBUG,
-            enable_renderer=render,
-            enable_vsync=False,
-            enable_recorder=False,
-        )
-        gym_env = make(
-            game=game,
-            reward_fn=CombinedReward(
-                (
-                    velocity_reward.VelocityPlayerToBallReward(),
-                    velocity_reward.VelocityBallToGoalReward(
-                        stadium=game.stadium_store, own_goal=False
-                    ),
-                )
-            ),
-            terminal_conditions=(
-                common_conditions.TimeoutCondition(int(1 * 60 * 60 / frame_skip)),
-                common_conditions.GoalScoredCondition(),
-            ),
-            obs_builder=DefaultObs(),
-            action_parser=DefaultAction(),
-            team_size=1,
-            tick_skip=frame_skip,
-        )
-        return gym_env._match
 
+def create_gym_env(render: bool):
+    game = Game(
+        stadium_file=cv.BaseMap.CLASSIC,
+        logging_level=logging.DEBUG,
+        enable_renderer=render,
+        enable_vsync=False,
+        enable_recorder=False,
+    )
+    gym_env = make(
+        game=game,
+        reward_fn=CombinedReward(
+            (
+                velocity_reward.VelocityPlayerToBallReward(),
+                velocity_reward.VelocityBallToGoalReward(
+                    stadium=game.stadium_store, own_goal=False
+                ),
+            )
+        ),
+        terminal_conditions=(
+            common_conditions.TimeoutCondition(int(1 * 60 * 60 / FRAME_SKIP)),
+            common_conditions.GoalScoredCondition(),
+        ),
+        obs_builder=DefaultObs(),
+        action_parser=DefaultAction(),
+        team_size=1,
+        tick_skip=FRAME_SKIP,
+    )
+    return gym_env._match
+
+
+def create_env(render: bool):
     # Set the first array argument as True to visualize progress.
     # Warning: performance are greatly impacted. I recommend to train without a GUI
     NB_GAMES = 8
@@ -56,20 +60,22 @@ if __name__ == "__main__":
         create_gym_env(False) for _ in range(NB_GAMES - 1)
     ]
 
-    fps = 60 / frame_skip
-    gamma = np.exp(np.log(0.5) / (fps * half_life_seconds))
+    fps = 60 / FRAME_SKIP
+    gamma = np.exp(np.log(0.5) / (fps * HALF_LIFE_SECONDS))
 
     env = SB3MultipleInstanceEnv(gym_env)
     env = VecCheckNan(env)
     env = VecMonitor(env)
     env = VecNormalize(env, norm_obs=False, gamma=gamma)
 
-    # Save a checkpoint every 100k steps
-    checkpoint_callback = CheckpointCallback(
-        save_freq=int(100_000 / env.num_envs),
-        save_path="logs/",
-        name_prefix="multi",
-    )
+    return env
+
+
+def model_new():
+    env = create_env(False)
+
+    fps = 60 / FRAME_SKIP
+    gamma = np.exp(np.log(0.5) / (fps * HALF_LIFE_SECONDS))
 
     # create a PPO instance and start learning
     model = PPO(
@@ -87,18 +93,39 @@ if __name__ == "__main__":
         device="auto",  # Uses GPU if available
     )
 
-    model.learn(100_000_000, callback=checkpoint_callback)
+    return model
 
-    # # load a model file
-    # model = PPO.load(
-    #     "logs/model.zip",
-    #     env,
-    #     custom_objects=dict(
-    #         n_envs=env.num_envs, _last_obs=None
-    #     ),  # Need this to change number of agents
-    #     device="auto",  # Need to set device again (if using a specific one)
-    # )
-    # env.reset()  # Important when loading models, SB3 does not do this for you
 
-    # # Use reset_num_timesteps=False to keep going with same logger/checkpoints
-    # model.learn(100_000_000, callback=checkpoint_callback, reset_num_timesteps=False)
+def model_continue(path_model: Path):
+    env = create_env(False)
+
+    # load a model file
+    model = PPO.load(
+        path_model,
+        env,
+        custom_objects=dict(
+            n_envs=env.num_envs, _last_obs=None
+        ),  # Need this to change number of agents
+        device="auto",  # Need to set device again (if using a specific one)
+    )
+    env.reset()  # Important when loading models, SB3 does not do this for you
+
+    return model, checkpoint_callback
+
+
+def main():
+    model = model_new()
+    # model = model_continue(Path("logs/model.zip"))
+
+    # Save a checkpoint every 100k steps
+    checkpoint_callback = CheckpointCallback(
+        save_freq=int(100_000 / NB_GAMES),
+        save_path="logs/",
+        name_prefix="multi",
+    )
+
+    model.learn(100_000_000, checkpoint_callback)
+
+
+if __name__ == "__main__":
+    main()
